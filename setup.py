4beb62295ef1bb0de7a310b6e123be90a8be7421
Weiqi Yan <weiqi_yan@outlook.com>
2024-11-21 14:23:59 +0000

    fix: make libtorch configuration optional.

--- setup.py
+++ setup.py
@@ -11,6 +11,17 @@ os.environ['MAX_JOBS'] = f"-j{max(int(os.cpu_count()*0.9), 1)}"
 setup_dir = os.path.dirname(os.path.realpath(__file__))
 HERE = pathlib.Path(__file__).absolute().parent
 
+def execute_cmake():
+    import subprocess, sys, shutil
+    cmake = shutil.which('cmake')
+    if cmake is None:
+        raise RuntimeError('Cannot find CMake executable.')
+    os.makedirs(os.path.join(HERE, 'build'), exist_ok=True)
+    retcode = subprocess.call([cmake, '..', '-Drequire_libtorch=OFF'], cwd=os.path.join(HERE, 'build'))
+    if retcode != 0:
+        sys.stderr.write("Error: CMake configuration failed.\n")
+        sys.exit(1)
+
 def remove_unwanted_pytorch_nvcc_flags():
     REMOVE_NVCC_FLAGS = [
         '-D__CUDA_NO_HALF_OPERATORS__',
@@ -31,27 +42,11 @@ def get_cuda_arch_flags():
         '-gencode', 'arch=compute_86,code=sm_86',  # Ampere
     ]
 
-def execute_cmake():
-    import subprocess, sys, shutil
-    cmake = shutil.which('cmake')
-    if cmake is None:
-        raise RuntimeError('Cannot find CMake executable.')
-    os.makedirs(os.path.join(HERE, 'build'), exist_ok=True)
-    retcode = subprocess.call([cmake, '--build', os.path.join(HERE, 'build')])
-    if retcode != 0:
-        sys.stderr.write("Error: CMake configuration failed.\n")
-        sys.exit(1)
-    
-    # install fast hadamard transform
-    # hadamard_dir = os.path.join(HERE, 'lib/fast-hadamard-transform')
-    # pip = shutil.which('pip')
-    # retcode = subprocess.call([pip, 'install', '-e', hadamard_dir])
 
 if __name__ == "__main__":
     execute_cmake()
     remove_unwanted_pytorch_nvcc_flags()
     os.makedirs(os.path.join(setup_dir, 'qlinear4bit'), exist_ok=True)
-    os.makedirs(os.path.join(setup_dir, 'packages', 'qlinear4bit'), exist_ok=True)
     setup(
         name='qlinear4bit',
         ext_modules=[
